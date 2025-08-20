e9efaeea4b327dd5ec85a5a12bcac233470d85ef
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-08-20 07:01:39 -0400

    Code cleanup

--- apps/shoppy-ui/src/app/layout.tsx
+++ apps/shoppy-ui/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import React from "react";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { Container, CssBaseline, ThemeProvider } from "@mui/material";
