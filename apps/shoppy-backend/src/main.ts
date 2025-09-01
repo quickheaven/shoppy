70855edaef060957fc84e53e886f1e1f575238dc
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-09-01 11:54:34 -0400

    15 JWT Strategy

--- apps/shoppy-backend/src/main.ts
+++ apps/shoppy-backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from 'nestjs-pino';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,9 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  app.use(cookieParser());
+
   await app.listen(app.get(ConfigService).getOrThrow('PORT'));
 }
 bootstrap().catch((err) => {
