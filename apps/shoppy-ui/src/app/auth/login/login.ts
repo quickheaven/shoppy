7622269e34d3fe8bf78cbc236a7cc12328b17943
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-09-01 16:01:20 -0400

    17 UI Login Auth

---
+++ apps/shoppy-ui/src/app/auth/login/login.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { FormError } from "@/app/common/types/interfaces/form-error.interface";
+import { API_URL } from "@/app/constants/api";
+import { getErrorMessage } from "@/app/util/errors";
+import { redirect } from "next/navigation";
+
+export default async function login(
+    _prevState: FormError,
+    formData: FormData,
+) {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(Object.fromEntries(formData)),
+  });
+  const parsedRes = await res.json();
+  if (!res.ok) {
+    return { error: getErrorMessage(parsedRes) };
+  }
+  redirect('/');
+}
