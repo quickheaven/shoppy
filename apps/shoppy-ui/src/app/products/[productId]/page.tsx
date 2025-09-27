e323e94feff12b0f1f0cb2908d5cf2c838982872
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-09-27 17:51:34 -0400

    34 Single Product UI

--- apps/shoppy-ui/src/app/products/[productId]/page.tsx
+++ apps/shoppy-ui/src/app/products/[productId]/page.tsx
@@ -1,10 +1,36 @@
+import { Grid, Stack, Typography } from "@mui/material";
+import getProduct from "./get-product";
+import Image from "next/image";
+import { getProductImage } from "../product-image";
+
 interface SingleProductProps {
-    params: {
-        productId: string;
-    };
+  params: { productId: string };
 }
 
+export default async function SingleProduct({ params }: SingleProductProps) {
+  const product = await getProduct(+params.productId);
 
-export default function SingleProduct({ params }: SingleProductProps) {
-    return <div>Single Product Page {params.productId}</div>;
+  return (
+    <Grid container marginBottom={"2rem"} rowGap={3}>
+      {product.imageExists && (
+        <Grid md={6} xs={12}>
+          <Image
+            src={getProductImage(product.id)}
+            width={0}
+            height={0}
+            className="w-full sm:w-3/4 h-auto"
+            sizes="100vw"
+            alt="Picture of the product"
+          />
+        </Grid>
+      )}
+      <Grid md={6} xs={12}>
+        <Stack gap={3}>
+          <Typography variant="h2">{product.name}</Typography>
+          <Typography>{product.description}</Typography>
+          <Typography variant="h4">${product.price}</Typography>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 }
