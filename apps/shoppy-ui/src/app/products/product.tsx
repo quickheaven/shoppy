acf22f8f617b5475648b3d14fb0afc15f4ed9621
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-09-27 16:17:48 -0400

    32 NextJS Image

--- apps/shoppy-ui/src/app/products/product.tsx
+++ apps/shoppy-ui/src/app/products/product.tsx
@@ -1,16 +1,30 @@
-import { Card, Typography } from "@mui/material";
+import { Card, Stack, Typography } from "@mui/material";
 import { Product as IProduct } from "./interfaces/product.interface";
+import Image from "next/image";
+import { API_URL } from "../common/constants/api";
 
 interface ProductProps {
-    product: IProduct;
+  product: IProduct;
 }
 
 export default function Product({ product }: ProductProps) {
-    return (
-        <Card className="p-4">
-            <Typography variant="h4">{product.name}</Typography>
-            <Typography>{product.description}</Typography>
-            <Typography>${product.price}</Typography>
-        </Card>
-    )
+  return (
+    <Card className="p-4">
+      <Stack gap={3}>
+        <Typography variant="h4">{product.name}</Typography>
+        {product.imageExists && (
+          <Image
+            src={`${API_URL}/products/${product.id}.jpg`}
+            width="0"
+            height="0"
+            className="w-full h-auto"
+            sizes="100vw"
+            alt="Picture of the product"
+          />
+        )}
+        <Typography>{product.description}</Typography>
+        <Typography>${product.price}</Typography>
+      </Stack>
+    </Card>
+  );
 }
